38e328ea455d8ea71f97ee6642ebcf93331ce3bf
MH Dip <mhdip05@gmail.com>
2022-06-04 22:32:03 +0600

    Authentication Validation

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
-
 
 @Component({
   selector: 'app-login',
@@ -11,11 +10,12 @@ import { AuthenticationService } from 'src/app/_services/authentication.service'
 })
 
 export class LoginComponent implements OnInit {
+  //@ViewChild('loginForm') myLoginForm!:ElementRef
   model: any = {}
 
   constructor(public authService: AuthenticationService,
-              private router:Router,
-              private toastr:ToastrService) { } 
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
     //console.log('currentUserr', this.authService.currentUser$)
@@ -23,14 +23,30 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    const isEmpty = Object.keys(this.model).length === 0;
+    if (isEmpty) {
+      this.toastr.error("Please enter your username and password")
+      return;
+    }
+
+    if (this.model.UserName == undefined) {
+      this.toastr.error("Please enter your username")
+      return
+    }
+
+    if (this.model.Password == undefined) {
+      this.toastr.error("Please enter your Password")
+      return
+    }
+
     this.authService.login(this.model).subscribe({
-       next: (v) => {
+      next: (v) => {
         this.router.navigateByUrl('/members')
-       },
-       error: (e)=>{
-         //console.log(e)
-         //this.toastr.error(e.error)
-       }
+      },
+      error: (e) => {
+        //console.log(e)
+        //this.toastr.error(e.error)
+      }
     })
   }
 
